cebdb64f6b2e6da730843d46d96593638f1cf5ee
AdamSvetec <adamsvetec@gmail.com>
2016-11-17 13:44:26 -0500

    flesh out lru cache functionality

--- lru_cache.c
+++ lru_cache.c
@@ -6,8 +6,30 @@ typedef struct{
   struct timeval time_of_use;
 }cache_file;
 
+void delete_cache_file(cache_file* file){
+  if(file == NULL){
+    return;
+  }
+  if(file->filename != NULL){
+    free(file->filename);
+  }
+  if(file->file_contents != NULL){
+    free(file->file_contents);
+  }
+  free(file);
+  file = NULL;
+}
+
+cache_file * create_cache_file(char * filename, void * file_contents, struct timeval time_of_use){
+  cache_file* file = malloc(sizeof(cache_file));
+  file->filename = filename;
+  file->file_contents = file_contents;
+  file->time_of_use = time_of_use;
+  return file;
+}
+
 typedef struct{
-  cache_file * files;
+  cache_file ** files;
   int size;
 }cache_wrap;
 
@@ -16,42 +38,49 @@ cache_wrap my_wrap;
 void intialize(int size){
   my_wrap.files = malloc(size * sizeof(cache_file*));
   my_wrap.size=size;
-  for( int i=0;i<size;i++){
-    my_wrap.files[i].filename=NULL;
+  for(int i=0;i<size;i++){
+    my_wrap.files[i] = NULL;
   }
 }
-cache_file get(int i){
-  return my_wrap.files[i];
+
+cache_file * get(char * filename){
+  for(int i = 0; i < my_wrap.size; i++){
+    if(my_wrap.files[i] != NULL){
+      if(strcmp(my_wrap.files[i]->filename, filename)==0){
+	return my_wrap.files[i];
+      }
+    }
+  }
+  return NULL;
 }
+
 //keeping these two things separate hopefully until we get it working. 
 int find_lru(){
   struct timeval oldest;
   gettimeofday(&oldest,NULL);
   //above just initializes the comparator time to most present
-  int oldest_index;
-  for( int i=0;i<my_wrap.size; i++){
-    struct timeval a= my_wrap.files[i].time_of_use; 
-    struct timeval b= oldest;
-    if(timercmp(&a, &b,<)){
-      struct timeval oldest= my_wrap.files[i].time_of_use;
-      oldest_index=i;
+  int oldest_index = -1;
+  for( int i=0; i<my_wrap.size; i++){
+    if(my_wrap.files[i] != NULL){
+      struct timeval a= my_wrap.files[i]->time_of_use; 
+      struct timeval b= oldest;
+      if(timercmp(&a, &b,<)){
+	struct timeval oldest= my_wrap.files[i]->time_of_use;
+	oldest_index=i;
+      }
     }
   }
   return oldest_index; 
 }
 
-void insert(cache_file cf){
-
+void insert(cache_file *cf){
   for( int i=0;i<my_wrap.size; i++){
-    if(my_wrap.files[i].filename==NULL){
-      my_wrap.files[i]=cf;
+    if(my_wrap.files[i] == NULL){
+      my_wrap.files[i] = cf;
       return;
     }
   }
   int lru=find_lru();
-  my_wrap.files[lru]=cf;
-  return;
+  delete_cache_file(my_wrap.files[lru]);
+  my_wrap.files[lru] = cf;
 }
-/*
-add_file
-*/
